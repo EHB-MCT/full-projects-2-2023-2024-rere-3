be20387c26b8b358aa8d513da9114698452e1e12
lars Otten <lars.otten@student.ehb.be>
2024-06-20 17:21:34 +0200

    eindpunt 1

--- script-hamburger.js
+++ script-hamburger.js
@@ -1,24 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+	var menuIcon = document.getElementById("menu-icon");
+	var menu = document.getElementById("menu");
+	var menuClose = document.getElementById("menu-close");
+	var menuItems = document.querySelectorAll(".menu-item");
 
-document.addEventListener('DOMContentLoaded', function() {
-    var menuIcon = document.getElementById('menu-icon');
-    var menu = document.getElementById('menu');
-    var menuClose = document.getElementById('menu-close');
-    var menuItems = document.querySelectorAll('.menu-item');
+	menuIcon.addEventListener("click", function () {
+		console.log("open");
+		menu.style.opacity = "1";
+		menu.style.visibility = "visible";
+	});
 
-    menuIcon.addEventListener('click', function() {
-        menu.style.opacity = '1';
-        menu.style.visibility = 'visible';
-    });
+	menuClose.addEventListener("click", function () {
+		console.log("close");
+		menu.style.opacity = "0";
+		menu.style.visibility = "hidden";
+	});
 
-    menuClose.addEventListener('click', function() {
-        menu.style.opacity = '0';
-        menu.style.visibility = 'hidden';
-    });
-
-    menuItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            menu.style.opacity = '0';
-            menu.style.visibility = 'hidden';
-        });
-    });
+	menuItems.forEach(function (item) {
+		item.addEventListener("click", function () {
+			console.log("close bc link");
+			menu.style.opacity = "0";
+			menu.style.visibility = "hidden";
+		});
+	});
 });
